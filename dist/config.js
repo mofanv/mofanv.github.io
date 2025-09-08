726c250f34453a1d146a0724158a0cf077285440
Mo, Fan Vincent <mofan1992@gmail.com>
2025-09-08 17:13:24 +0800

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -1,6 +1,5 @@
 // Configuration file for WASM agents
 window.APP_CONFIG = {
-    DEEPSEEK_API_KEY: '', // deepseek api key
     LLM_MODE: 'deepseek', // 或 "local" 使用本地API
     EMBEDDING_MODE: "transformersjs", // 或 "local" 使用本地API
     TOKENIZERS_PARALLELISM: false
