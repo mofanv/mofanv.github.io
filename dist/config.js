bde9d6ac1ecf3dbe16ea556a4bc7f2fb9792935c
Mo, Fan Vincent <mofan1992@gmail.com>
2025-09-21 09:34:58 +0800

    fetch web data function

--- dist/config.js
+++ dist/config.js
@@ -1,7 +1,7 @@
 // Configuration file for WASM agents
 window.APP_CONFIG = {    
     DEEPSEEK_API_KEY: 'sk-5a608132a9e64102b448b153c6434b49', // deepseek api key
-    OPENAI_API_KEY: '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', // openai api key    LLM_MODE: 'deepseek', // 或 "local" 使用本地API
+    OPENAI_API_KEY: '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', // openai api key    LLM_MODE: 'deepseek', // 或 "local" 使用本地API
     LLM_MODE: 'deepseek', // 或 "local" 使用本地API
     EMBEDDING_MODE: "transformersjs", // 或 "local" 使用本地API
     TOKENIZERS_PARALLELISM: false
