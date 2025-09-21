038d84b50fa8fd01f396d2d34065b11d5fbf4dc6
Mo, Fan Vincent <mofan1992@gmail.com>
2025-09-21 08:46:27 +0800

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -2,6 +2,7 @@
 window.APP_CONFIG = {    
     DEEPSEEK_API_KEY: 'sk-5a608132a9e64102b448b153c6434b49', // deepseek api key
     OPENAI_API_KEY: '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', // openai api key    LLM_MODE: 'deepseek', // 或 "local" 使用本地API
+    LLM_MODE: 'deepseek', // 或 "local" 使用本地API
     EMBEDDING_MODE: "transformersjs", // 或 "local" 使用本地API
     TOKENIZERS_PARALLELISM: false
 };
